63375dd6be39c24b6eef6ba2c2581df62c7a73ae
damien messner <damien.messner@gmail.com>
2021-06-30 22:42:52 +0200

    commit

---
+++ temperature/code.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // the number of temperatures to analyse
+
+    int valNeg = -6000;
+    int  valPos = 6000;
+    int  valFinal = 0;
+   
+
+        for (int i = 0; i < n; i++) {
+            int t = in.nextInt(); // a temperature expressed as an integer ranging from -273 to 5526
+            if (t < 0 && t > valNeg )
+        {
+            valNeg = t;
+        }
+        if (t > 0 && t < valPos )
+        {
+            valPos = t;
+        }
+        if(valPos == -valNeg || valPos < -valNeg)
+        {
+            valFinal = valPos;
+        }
+        else
+        {
+            valFinal = valNeg;
+        }
+        System.err.println("Debug message..."+t);
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(valFinal);
+    }
+}
